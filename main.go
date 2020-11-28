fce2a4ce9969d6341f076a14e52021f7068fdc0a
mizu0 <mizu0@iMac.local>
2020-11-28 20:48:31 +0900

    change path

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello World!!!!")
+	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hello World!")
 	})
 
 
