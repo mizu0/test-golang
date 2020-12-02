ba95502222803f59ff40c0ebc7d59d07978757d7
mizu0 <mizu0@iMac.local>
2020-12-02 17:32:11 +0900

    change routing.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "Hello World!")
 	})
 
