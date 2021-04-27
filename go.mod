cdda8ad7ab96efbe5b0f3395065fbb47927c2d3d
mizu0 <mizu0@iMac.local>
2021-04-27 14:48:22 +0900

    add.

--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module github.com/mizu0/test-service
 go 1.15
 
 require (
+	cloud.google.com/go/spanner v1.17.0
+	github.com/GoogleCloudPlatform/golang-samples v0.0.0-20210420143725-d791483f25a2 // indirect
 	github.com/aws/aws-lambda-go v1.20.0
-	github.com/aws/aws-sdk-go v1.35.35
+	github.com/aws/aws-sdk-go v1.38.21
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	golang.org/x/net v0.0.0-20210420072503-d25e30425868 // indirect
+	golang.org/x/sys v0.0.0-20210420072515-93ed5bcd2bfe // indirect
+	google.golang.org/api v0.45.0 // indirect
+	google.golang.org/genproto v0.0.0-20210420162539-3c870d7478d2 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"cloud.google.com/go/spanner"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+	dataClient, err := spanner.NewClient(ctx, db)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	_, err = client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		stmt := spanner.Statement{
+			SQL: `INSERT Singers (SingerId, FirstName, LastName) VALUES
+                                (12, 'Melissa', 'Garcia'),
+                                (13, 'Russell', 'Morales'),
+                                (14, 'Jacqueline', 'Long'),
+                                (15, 'Dylan', 'Shaw')`,
+		}
+		rowCount, err := txn.Update(ctx, stmt)
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(w, "%d record(s) inserted.\n", rowCount)
+		return err
+	})
+	return err
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World!")
 	})
-
 
 	fmt.Println("Starting server at port 8000")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
